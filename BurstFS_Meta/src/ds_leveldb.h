a696b44abb41b55d6890cd8d995f21ee2f4af70d
Danielle Sikich <sikich1@llnl.gov>
2017-06-12 12:51:59 -0700

    Create ds_leveldb.h

--- BurstFS_Meta/src/ds_leveldb.h
+++ BurstFS_Meta/src/ds_leveldb.h
@@ -1,16 +1,23 @@
 /*
-* Copyright (c) 2017, Lawrence Livermore National Security, LLC.
-* Produced at the Lawrence Livermore National Laboratory.
-* Copyright (c) 2017, Florida State University. Contributions from
-* the Computer Architecture and Systems Research Laboratory (CASTL)
-* at the Department of Computer Science.
-*
-* Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
-* LLNL-CODE-728877. All rights reserved.
-*
-* This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
-* Please read https://github.com/llnl/burstfs/LICENSE for full license text.
+ * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Copyright (c) 2017, Florida State University. Contributions from
+ * the Computer Architecture and Systems Research Laboratory (CASTL)
+ * at the Department of Computer Science.
+ *
+ * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
+ * LLNL-CODE-728877. All rights reserved.
+ *
+ * This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
+ * Please read https://github.com/llnl/burstfs/LICENSE for full license text.
 */
+
+/*
+ *
+ * Copyright (c) 2014, Los Alamos National Laboratory
+ *	All rights reserved.
+ *
+ */
 
 #ifndef      __LEVELDB_H
 #define      __LEVELDB_H
