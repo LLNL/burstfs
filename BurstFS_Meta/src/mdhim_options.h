55de8189b678dc67e465b716517989f97c169c9d
Danielle Sikich <sikich1@llnl.gov>
2017-06-12 10:35:02 -0700

    Update mdhim_options.h

--- BurstFS_Meta/src/mdhim_options.h
+++ BurstFS_Meta/src/mdhim_options.h
@@ -1,14 +1,16 @@
 /*
- *
- * Copyright (c) 2014, Los Alamos National Laboratory
- *	All rights reserved.
- *
- */
-
-/*
- * DB usage options. 
- * Location and name of DB, type of DataSotre primary key type,
- */
+* Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+* Produced at the Lawrence Livermore National Laboratory.
+* Copyright (c) 2017, Florida State University. Contributions from
+* the Computer Architecture and Systems Research Laboratory (CASTL)
+* at the Department of Computer Science.
+*
+ * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
+* LLNL-CODE-728877. All rights reserved.
+*
+ * This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
+* Please read https://github.com/llnl/burstfs/LICENSE for full license text.
+*/
 
 #ifndef      __OPTIONS_H
 #define      __OPTIONS_H
