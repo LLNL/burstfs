c4381f3a42946abe685a0d30a6f09a132f64395f
Danielle Sikich <sikich1@llnl.gov>
2017-06-12 10:29:49 -0700

    Update uthash.h
    shouldn't have changed this one to LLNL license

--- BurstFS_Client/src/uthash.h
+++ BurstFS_Client/src/uthash.h
@@ -1,15 +1,21 @@
 /*
-* Copyright (c) 2017, Lawrence Livermore National Security, LLC.
-* Produced at the Lawrence Livermore National Laboratory.
-* Copyright (c) 2017, Florida State University. Contributions from
-* the Computer Architecture and Systems Research Laboratory (CASTL)
-* at the Department of Computer Science.
-*
- * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
-* LLNL-CODE-728877. All rights reserved.
-*
- * This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
-* Please read https://github.com/llnl/burstfs/LICENSE for full license text.
+Copyright (c) 2003-2012, Troy D. Hanson     http://uthash.sourceforge.net
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #ifndef UTHASH_H
