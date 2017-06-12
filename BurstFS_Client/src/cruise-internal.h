9d608256556b50234d9c0bd1dc3005fcd3102346
Danielle Sikich <sikich1@llnl.gov>
2017-06-12 10:42:47 -0700

    Create cruise-internal.h

--- BurstFS_Client/src/cruise-internal.h
+++ BurstFS_Client/src/cruise-internal.h
@@ -5,10 +5,10 @@
 * the Computer Architecture and Systems Research Laboratory (CASTL)
 * at the Department of Computer Science.
 *
- * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
+* Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
 * LLNL-CODE-728877. All rights reserved.
 *
- * This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
+* This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
 * Please read https://github.com/llnl/burstfs/LICENSE for full license text.
 */
 
