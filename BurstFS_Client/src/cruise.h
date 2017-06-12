77c9badeb5eb4e19c7fddfe99fb4107132f76595
Danielle Sikich <sikich1@llnl.gov>
2017-06-12 13:12:05 -0700

    Update cruise.h

--- BurstFS_Client/src/cruise.h
+++ BurstFS_Client/src/cruise.h
@@ -1,28 +1,28 @@
 /*
- * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- * Copyright (c) 2017, Florida State University. Contributions from
- * the Computer Architecture and Systems Research Laboratory (CASTL)
- * at the Department of Computer Science.
- *
- * Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
- * LLNL-CODE-728877. All rights reserved.
- *
- * This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
- * Please read https://github.com/llnl/burstfs/LICENSE for full license text.
+* Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+* Produced at the Lawrence Livermore National Laboratory.
+* Copyright (c) 2017, Florida State University. Contributions from
+* the Computer Architecture and Systems Research Laboratory (CASTL)
+* at the Department of Computer Science.
+*
+* Written by: Teng Wang, Adam Moody, Weikuan Yu, Kento Sato, Kathryn Mohror
+* LLNL-CODE-728877. All rights reserved.
+*
+* This file is part of BurstFS. For details, see https://github.com/llnl/burstfs
+* Please read https://github.com/llnl/burstfs/LICENSE for full license text.
 */
 
 /*
- * Copyright (c) 2013, Lawrence Livermore National Security, LLC.
- * Produced at the Lawrence Livermore National Laboratory.
- * code Written by
- *   Raghunath Rajachandrasekar <rajachan@cse.ohio-state.edu>
- *   Kathryn Mohror <kathryn@llnl.gov>
- *   Adam Moody <moody20@llnl.gov>
- * All rights reserved.
- * This file is part of CRUISE.
- * For details, see https://github.com/hpc/cruise
- * Please also read this file COPYRIGHT
+* Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+* Produced at the Lawrence Livermore National Laboratory.
+* code Written by
+*   Raghunath Rajachandrasekar <rajachan@cse.ohio-state.edu>
+*   Kathryn Mohror <kathryn@llnl.gov>
+*   Adam Moody <moody20@llnl.gov>
+* All rights reserved.
+* This file is part of CRUISE.
+* For details, see https://github.com/hpc/cruise
+* Please also read this file LICENSE.CRUISE 
 */
 
 #ifndef CRUISE_H
