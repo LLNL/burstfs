9da90fdd80361cc59853488d04f009d0f8d74cf3
Danielle Sikich <sikich1@llnl.gov>
2017-06-14 21:29:00 -0700

    dsikich/license update (#1)
    * Create LICENSE
    
    * Delete LICENSE
    
    * Create LICENSE
    
    * Create LICENSE
    
    * Create LICENSE
    
    * Create LICENSE
    
    * Create cruise-fixed.h
    
    * Create cruise-internal.h
    
    * Create cruise-stack.h
    
    * Create cruise-stdio.h
    
    * Update cruise-sysio.h
    
    * Update cruise.h
    
    * Update uthash.h
    
    * Update uthash.h
    
    shouldn't have changed this one to LLNL license
    
    * Update client.h
    
    * Update data_store.h
    
    * Update ds_leveldb.h
    
    * Update ds_mysql.h
    
    * Update indexes.h
    
    * Create indexes.h
    
    removing unnecessary comment
    
    * Update local_client.h
    
    * Update mdhim.h
    
    * Update mdhim_options.h
    
    * Update mdhim_private.h
    
    * Update messages.h
    
    * Update partitioner.h
    
    * Create range_server.h
    
    * Create client.h
    
    * Create data_store.h
    
    * Create mdhim_options.h
    
    * Create client.h
    
    * Create data_store.h
    
    * Create ds_leveldb.h
    
    * Create ds_mysql.h
    
    * Create indexes.h
    
    * Create local_client.h
    
    * Create mdhim.h
    
    * Create mdhim_private.h
    
    * Create partitioner.h
    
    * Create range_server.h
    
    * Create cruise-fixed.h
    
    * Create cruise-internal.h
    
    * Create cruise-stack.h
    
    * Create cruise-stdio.h
    
    * Create cruise-sysio.h
    
    * Create cruise.h
    
    * Create arraylist.h
    
    * Create burstfs_cmd_handler.h
    
    * Create burstfs_const.h
    
    * Create burstfs_debug.h
    
    * Create burstfs_global.h
    
    * Create burstfs_init.h
    
    * Create burstfs_metadata.h
    
    * Create burstfs_request_manager.h
    
    * Update burstfs_service_manager.h
    
    * Update burstfs_setup.h
    
    * Create burstfs_setup.h
    
    * Create burstfs_service_manager.h
    
    * Create burstfs_sock.h
    
    * Create log.h
    
    * adding README from old burstfs version in bitbucket
    
    * Create client.h
    
    * Update data_store.h
    
    * Create ds_leveldb.h
    
    * Update ds_mysql.h
    
    * Update indexes.h
    
    * Update local_client.h
    
    * Update mdhim.h
    
    * Update mdhim_options.h
    
    * Update mdhim_private.h
    
    * Update messages.h
    
    * Update partitioner.h
    
    * Update range_server.h
    
    * Update cruise-fixed.h
    
    * Update cruise-internal.h
    
    * Update cruise-stack.h
    
    * Update cruise-stdio.h
    
    * Update cruise-sysio.h
    
    * Update cruise.h
    
    * Update cruise-fixed.h
    
    * Update cruise-internal.h
    
    * Update cruise-stack.h
    
    * Update cruise-stdio.h
    
    * Update cruise-sysio.h
    
    * Update cruise.h
    
    * Update client.h
    
    spaces bothering me
    
    * Update data_store.h
    
    * Update ds_leveldb.h
    
    * Update ds_mysql.h
    
    * Update indexes.h
    
    * Update local_client.h
    
    * Create mdhim.h
    
    * Update mdhim_options.h
    
    * Update mdhim_private.h
    
    * Update messages.h
    
    * Update partitioner.h
    
    * Update range_server.h
    
    * Update cruise-fixed.c
    
    * Update cruise-stack.c
    
    * Update cruise-stdio.c
    
    * Update cruise-sysio.c
    
    * Update cruise.c
    
    * Update client.c
    
    * Update data_store.c
    
    * Update ds_leveldb.c
    
    * Update ds_mysql.c
    
    * Update indexes.c
    
    * Update local_client.c
    
    * Create mdhim.c
    
    * Update mdhim_options.c
    
    * Update mdhim_private.c
    
    * Update messages.c
    
    * Update partitioner.c
    
    * Update range_server.c
    
    * Update arraylist.c
    
    * Update burstfs_cmd_handler.c
    
    * Update burstfs_const.c
    
    * Update burstfs_debug.c
    
    * Update burstfs_init.c
    
    * Update burstfs_metadata.c
    
    * Update burstfs_request_manager.c
    
    * Update burstfs_service_manager.c
    
    * Update burstfs_sock.c

--- BurstFS_Client/src/uthash.h
+++ BurstFS_Client/src/uthash.h
@@ -1,13 +1,10 @@
 /*
 Copyright (c) 2003-2012, Troy D. Hanson     http://uthash.sourceforge.net
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
 IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
